52e41db47156228e6f3a8b9faba87eaee0f74d32
Dodic-Codes <119444964+Dodic-Codes@users.noreply.github.com>
2022-12-13 20:47:09 +0400

    Update install.bash

--- install.bash
+++ install.bash
@@ -9,6 +9,7 @@ curl -fsSL https://download.docker.com/linux/ubuntu/gpg | gpg --dearmor -o /etc/
 echo \
   "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
   $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+apt-get install runit-systemd
 apt-get update
-apt-get install runit-systemd docker-ce docker-ce-cli containerd.io docker-compose-plugin
+apt-get install docker-ce docker-ce-cli containerd.io docker-compose-plugin
 docker run hello-world
